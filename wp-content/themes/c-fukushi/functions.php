2e5845b22dfb861060e70ad813db7777427b3699
TAIRA SHUNSUKE <suke.envs1@gmail.com>
2025-11-14 16:27:22 +0900

    スラッグ'ichinichi'、'ichinichi-hana'の修正とスラッグ'futsuka'のマークアップ化

--- wp-content/themes/c-fukushi/functions.php
+++ wp-content/themes/c-fukushi/functions.php
@@ -1393,7 +1393,7 @@ function custom_schema_ichinichi_detail() {
                 "step": [
                     {
                         "@type": "HowToStep",
-                        "name": "ご逝去・搬送・打合せ（1日目）",
+                        "name": "ご逝去・搬送・打合せ",
                         "text": "病院等でご逝去後、中央福祉葬祭へ連絡し、ご遺体を安置場所へ搬送します。ご安置後、日程やプラン内容の詳細を決定します。通夜は行いません。"
                     },
                     {
@@ -1629,12 +1629,12 @@ function custom_schema_ichinichi_hana_detail() {
                 "step": [
                     {
                         "@type": "HowToStep",
-                        "name": "ご逝去・搬送・打合せ（1日目）",
+                        "name": "ご逝去・搬送・打合せ",
                         "text": "病院等でご逝去後、中央福祉葬祭へ連絡し、ご遺体を安置場所へ搬送します。ご安置後、日程やプラン内容の詳細を決定します。通夜は行いません。"
                     },
                     {
                         "@type": "HowToStep",
-                        "name": "納棺・告別式・火葬（2日目）",
+                        "name": "納棺・告別式・火葬（1日目）",
                         "text": "納棺後、告別式・葬儀を執り行います。その後、火葬場へ出棺し、火葬・収骨を行います。"
                     }
                 ]
@@ -1826,4 +1826,279 @@ JSON;
     }
 }
 add_action('wp_head', 'custom_schema_ichinichi_hana_detail');
+?>
+<?php
+// schema_futsuka
+function custom_schema_futsuka_detail() {
+    if ( is_page('futsuka') ) {
+        $schema_json = <<<JSON
+[
+    {
+        "@context": "https://schema.org",
+        "@graph": [
+            {
+                "@type": "Service",
+                "@id": "https://staging.c-fukushi.co.jp/plan/futsuka/#service",
+                "serviceType": "家族葬二日プラン",
+                "name": "中央福祉葬祭の家族葬二日プラン",
+                "description": "ご遺族様を中心とした親しい方だけで、通夜・葬儀を2日間でおこなうプランです。「家族葬で通夜・葬儀をきちんと営みたい方」「多くの会葬者様とゆっくりお見送りしたい方」といった方に選ばれています。",
+                "provider": {
+                    "@type": "Organization",
+                    "name": "中央福祉葬祭",
+                    "url": "https://staging.c-fukushi.co.jp/"
+                },
+                "areaServed": [
+                    { "@type": "City", "name": "川口市" },
+                    { "@type": "City", "name": "蕨市" },
+                    { "@type": "State", "name": "埼玉県" }
+                ],
+                "hasOfferCatalog": {
+                    "@type": "OfferCatalog",
+                    "name": "家族葬二日プランの価格情報",
+                    "itemListElement": [
+                        {
+                            "@type": "Offer",
+                            "name": "家族葬二日プラン",
+                            "priceCurrency": "JPY",
+                            "price": "598000",
+                            "url": "http://testcfukushi.local/plan/futsuka/",
+                            "availability": "https://schema.org/InStock"
+                        }
+                    ]
+                }
+            },
+            {
+                "@type": "HowTo",
+                "@id": "https://staging.c-fukushi.co.jp/plan/futsuka/#howto",
+                "name": "家族葬二日プランの流れ（手順）",
+                "description": "家族葬二日プランにおけるご逝去から通夜・葬儀までの基本的な流れです。",
+                "step": [
+                    {
+                        "@type": "HowToStep",
+                        "name": "ご逝去・搬送・打合せ",
+                        "text": "病院等でご逝去後、中央福祉葬祭へ連絡し、ご遺体を安置場所へ搬送します。ご安置後、日程やプラン内容の詳細を決定します。"
+                    },
+                    {
+                        "@type": "HowToStep",
+                        "name": "納棺・通夜（1日目）",
+                        "text": "納棺を行い、通夜式を執り行います。"
+                    },
+                    {
+                        "@type": "HowToStep",
+                        "name": "葬儀・告別式・火葬（2日目）",
+                        "text": "葬儀・告別式を執り行います。その後、火葬場へ出棺し、火葬・収骨を行います。"
+                    }
+                ]
+            },
+            {
+                "@type": "ItemList",
+                "@id": "https://staging.c-fukushi.co.jp/plan/futsuka/#plan_item_list",
+                "name": "家族葬二日プランに含まれる内容",
+                "itemListElement": [
+                    {
+                        "@type": "ListItem",
+                        "position": 1,
+                        "item": {
+                            "@type": "Service",
+                            "name": "ご搬送(10km)"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 2,
+                        "item": {
+                            "@type": "Service",
+                            "name": "ご安置（2日分）"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 3,
+                        "item": {
+                            "@type": "Service",
+                            "name": "保冷剤（2日分）"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 4,
+                        "item": {
+                            "@type": "Service",
+                            "name": "枕飾り"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 5,
+                        "item": {
+                            "@type": "Service",
+                            "name": "事務手続き代行"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 6,
+                        "item": {
+                            "@type": "Service",
+                            "name": "御棺(桐六分)"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 7,
+                        "item": {
+                            "@type": "Service",
+                            "name": "シーツ"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 8,
+                        "item": {
+                            "@type": "Service",
+                            "name": "運営管理費"
+                        }
+                    },
+					
+                    {
+                        "@type": "ListItem",
+                        "position": 9,
+                        "item": {
+                            "@type": "Service",
+                            "name": "仏衣"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 10,
+                        "item": {
+                            "@type": "Service",
+                            "name": "道路案内"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 11,
+                        "item": {
+                            "@type": "Service",
+                            "name": "御遺影写真(カラー)"
+                        }
+                    },
+					
+                    {
+                        "@type": "ListItem",
+                        "position": 12,
+                        "item": {
+                            "@type": "Service",
+                            "name": "白木位牌"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 13,
+                        "item": {
+                            "@type": "Service",
+                            "name": "式場設備"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 14,
+                        "item": {
+                            "@type": "Service",
+                            "name": "ローソク・線香"
+                        }
+                    },
+					
+                    {
+                        "@type": "ListItem",
+                        "position": 15,
+                        "item": {
+                            "@type": "Service",
+                            "name": "祭壇"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 16,
+                        "item": {
+                            "@type": "Service",
+                            "name": "司会進行"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 17,
+                        "item": {
+                            "@type": "Service",
+                            "name": "火葬場案内"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 18,
+                        "item": {
+                            "@type": "Service",
+                            "name": "ご搬送(火葬場まで)"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 19,
+                        "item": {
+                            "@type": "Service",
+                            "name": "骨壺"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 20,
+                        "item": {
+                            "@type": "Service",
+                            "name": "ご自宅飾り"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 21,
+                        "item": {
+                            "@type": "Service",
+                            "name": "花祭壇"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 22,
+                        "item": {
+                            "@type": "Service",
+                            "name": "事務用品一式・受付"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 20,
+                        "item": {
+                            "@type": "Service",
+                            "name": "通夜・葬儀"
+                        }
+                    },
+                    {
+                        "@type": "ListItem",
+                        "position": 21,
+                        "item": {
+                            "@type": "Service",
+                            "name": "会葬礼状(50枚)"
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+]
+JSON;
+
+        echo '<script type="application/ld+json">' . $schema_json . '</script>';
+    }
+}
+add_action('wp_head', 'custom_schema_futsuka_detail');
 ?>
